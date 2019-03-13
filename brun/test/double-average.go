1523b802498948d2237469bd41b63d34bf9d9d73
15555 <chengang.xu@hand-china.com>
2019-03-13 08:22:38 +0800

    增加二分算法和两倍随机数算法及测试用例

---
+++ brun/test/double-average.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"xcg.com/resk/infra/algo"
+)
+
+func main() {
+	count, amount := int64(10), int64(100)
+
+	for i := int64(0); i < count; i++ {
+		x := algo.DoubleAverage(count, amount*100)
+		fmt.Print(x, ",")
+	}
+	fmt.Println()
+}
